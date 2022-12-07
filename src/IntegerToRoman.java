ea8c69ab70da6cde3c583f63022d954e3c8f8d5b
Danil Belousov <eternitytinrete@mail.ru>
2022-12-07 20:15:36 +0300

    Commit

--- src/IntegerToRoman.java
+++ src/IntegerToRoman.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class IntegerToRoman {
-}
+ class IntegerToRoman {
+     public String intToRoman(int num)
+     {
+
+         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+         String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+         StringBuilder roman = new StringBuilder();
+         for(int i=0;i<values.length;i++)
+         {
+             while(num >= values[i])
+             {
+                 num = num - values[i];
+                 roman.append(romanLetters[i]);
+             }
+         }
+         return roman.toString();
+     }
+
+
+
+ }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,80 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String expression = br.readLine();
+        System.out.println(calc(expression));
+    }
+    public static String calc(String input) throws Exception {
+        IntegerToRoman toRoman = new IntegerToRoman();
+        RomanToInteger toInteger = new RomanToInteger();
+        int result = 0;
+        int x1 = 0;
+        int x2 = 0;
+        String[] NumberArray = input.split(" ");
+        if (isRome(NumberArray[0]) & isRome(NumberArray[2])) {
+            x1 = toInteger.romanToInteger(NumberArray[0]);
+            x2 = toInteger.romanToInteger(NumberArray[2]);
+            switch (NumberArray[1]) {
+                case "+":
+                    result = (x1 + x2);
+                    break;
+                case "-":
+                    result = (x1 - x2);
+                    break;
+                case "*":
+                    result = (x1 * x2);
+                    break;
+                case "/":
+                    result = (x1 / x2);
+                    break;
+            }
+            if (result <= 0) {
+                throw new Exception();
+            }
+            return toRoman.intToRoman(result);
+        }
+       else if (isArabic(NumberArray[0]) & isArabic(NumberArray[2])) {
+            x1 = Integer.parseInt(NumberArray[0]);
+            x2 = Integer.parseInt(NumberArray[2]);
+            switch (NumberArray[1]) {
+                case "+":
+                    result = (x1 + x2);
+                    break;
+                case "-":
+                    result = (x1 - x2);
+                    break;
+                case "*":
+                    result = (x1 * x2);
+                    break;
+                case "/":
+                    result = (x1 / x2);
+                    break;
+            }
+            return Integer.toString(result);
+        }
+        else {throw new Exception();
+        }
+    }
+    public static boolean isArabic (String input) {
+       String[] arabic = {"1","2","3","4","5","6","7","8","9","10"};
+        for (String an : arabic) {
+            if (input.equals(an)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean isRome (String input) {
+        String[] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        for (String rn : romans) {
+            if (input.equals(rn)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
